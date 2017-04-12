1f490b49bae583dd2a667140fc32dd2cc97b04c0
ваше имя < >
2017-04-12 21:52:36 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,4 +1,12 @@
 public class Calculate {
-public static void main(String[] arg){
-System.out.println("Calculate...");}
+  public static void main(String[] arg){
+	System.out.println("Calculate...");
+	int first = Integer.valueOf(arg[0]);
+	int second = Integer.valueOf(arg[1]);
+	int third = Integer.valueOf(arg[2]);
+	int summ = first + second;
+	int multiplication = first * third; 
+	System.out.println("Sum   " + summ);
+	System.out.println("Multi   " + multiplication);
+  }
 }
